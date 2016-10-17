51173b7795c98abd9e59eaccf01b4cd088f79691
Bertrand Prime <Bebzh@macbook-pro-de-bertrand.home>
2016-10-17 23:37:51 +0200

    added authent

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
+
+# Create your views here.
+# this login required decorator is to not allow to any
+# view without authenticating
+@login_required(login_url="login/")
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'post_list.html', {'posts': posts})
--- kiteinbzh/urls.py
+++ kiteinbzh/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views
+from blog.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm}, name='login'),
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),
 ]
